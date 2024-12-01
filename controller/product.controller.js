c30c8f01aab84e6835f60411045c834c790f4356
Deepanshu-Mehta <146458164+Deepanshu-Mehta@users.noreply.github.com>
2024-12-02 00:45:16 +0530

    updated

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -6,7 +6,7 @@ const createProduct = async (req,res)=>{
             return res.status(400).json({error: "All fields required"})
         }
         const product = await prisma.Product.create({
-            data : {name,stock : parseInt(stock),price : Number(price)}
+            data : {name, stock, price}
         });
         res.status(201).json(product);
     }catch(err){
@@ -40,14 +40,13 @@ const retrivebyId= async (req,res)=>{
 const updateProduct = async (req,res)=>{
     try{
     const {id} = req.params;
-    const {name,stock,price} = req.body;
     const product = await prisma.Product.findUnique({where : {id : parseInt(id)}});
     if(!product){
         return res.status(404).json({error : "Product not found"});
     }
     const updatedProduct = await prisma.Product.update({
         where : {id : parseInt(id)},
-        data : {name,stock : parseInt(stock),price : Number(price)}
+        data : req.body
     })
     res.status(200).json(updatedProduct);
     }catch(err){
@@ -62,14 +61,7 @@ const partialUpdate = async(req,res)=>{
     if(!product){
         return res.status(404).json({error : "Product not found"});
     }
-    const updateData = {...req.body};
-    if (updateData.stock) updateData.stock = parseInt(updateData.stock);
-    if (updateData.price) updateData.price = Number(updateData.price);
-
-    const updatedProduct = await prisma.Product.update({
-        where : {id : parseInt(id)}, 
-        data : updateData
-    });
+    const updatedProduct = await prisma.Product.update({where : {id : parseInt(id)}, data : req.body});
     res.status(200).json(updatedProduct);
     }catch(err){
         return res.status(500).json({message : "Internal Server Error"});
